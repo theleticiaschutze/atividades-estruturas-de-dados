12859d72da5a72e2831ac20a39c67aebb7decb6e
Letícia Schütze <the.leticia.schutze@gmail.com>
2026-05-28 16:27:23 -0300

    Trabalho de menu

--- lista - struct/menu-pesquisa/main.c
+++ lista - struct/menu-pesquisa/main.c
@@ -7,7 +7,6 @@ int peganumero()
 {
    int op;
    system("cls");
-   //esse щ um comando do system(sistema) cmd que limpa a tela toda
    printf("CADASTRO DE ALUNOS\n");
    printf("[0] - Sair\n");
    printf("[1] - Inclusao\n");
@@ -25,17 +24,22 @@ typedef struct {
    float alt;
 } tAluno;
 
+void meuStrToUpper(char *str)
+{
+   for (int i=0; i<strlen(str); i++) {
+      if(str[i]>='a' && str[i]<='z') {
+         str[i] -= 32;
+      }
+   }
+}
+
 int main()
 {
-   int op, qtd=0, i, encontrados;
+   int op, qtd=0, i=0, encontrados=0;
    tAluno vAluno[MAX];
    char pesq[MAX];
-   char *recorte; //ponteiro para usar no strstr
-
 
    do {
-      //bloco para if/else ou switch case
-      //para tratar a opção selecionada
       op = peganumero();
 
       switch(op) {
@@ -44,12 +48,12 @@ int main()
          printf("Saindoo...\n");
          break;
       case 1:
-         printf("\n--- INCLUSAO ---\n");
+         printf("\n------- INCLUSAO -------\n");
          printf("Matricula: ");
          scanf("%d", &vAluno[qtd].matr);
          printf("Nome: ");
          scanf(" %[^\n]", vAluno[qtd].nome);
-         strcpy(vAluno[qtd].nome, strupr(vAluno[qtd].nome));
+         meuStrToUpper(vAluno[qtd].nome);
          printf("Sexo: ");
          scanf(" %c", &vAluno[qtd].sexo);
          printf("Altura: ");
@@ -58,7 +62,7 @@ int main()
          printf("Matricula incluida!\n");
          break;
       case 2:
-         printf("\n--- LISTAGEM ---\n");
+         printf("\n------- LISTAGEM ------\n");
          if (qtd == 0) {
             printf("Nenhum aluno cadastrado.\n");
          }
@@ -67,8 +71,7 @@ int main()
             printf("| Matr | Nome                 | Sex | Altura |\n");
             printf("+------+----------------------+-----+--------+\n");
 
-            for(i = 0; i < qtd; i++) { //um loop para mostrar cada aluno no vetor
-               // Imprime cada linha da tabela com os dados do aluno atual
+            for(i = 0; i < qtd; i++) {
                printf("| %4d | %-20s |  %c  | %6.2f |\n", vAluno[i].matr, vAluno[i].nome, vAluno[i].sexo, vAluno[i].alt);
             }
             printf("+------+----------------------+-----+--------+\n");
@@ -77,29 +80,32 @@ int main()
          }
          break;
       case 3:
-         printf("Para perquisar digite parte do nome que quer encontrar!\n");
+         encontrados = 0;
+         printf("\n------- PESQUISA ------\n");
+         printf("Para pesquisar digite parte do nome que quer encontrar!\n");
          scanf(" %[^\n]", pesq);
-         strcpy(pesq, strupr(pesq));
 
-         for(i=0; i<=qtd; i++){
-            recorte = strstr (vAluno[i].nome, pesq);
+         meuStrToUpper(pesq);
+
+         printf("+------+----------------------+-----+--------+\n");
+         printf("| Matr | Nome                 | Sex | Altura |\n");
+         printf("+------+----------------------+-----+--------+\n");
+
+         for(i = 0; i < qtd; i++) {
+            if(strstr(vAluno[i].nome, pesq) != NULL) {
+               printf("| %4d | %-20s |  %c  | %6.2f |\n", vAluno[i].matr, vAluno[i].nome, vAluno[i].sexo, vAluno[i].alt);
+               encontrados++;
+            }
          }
 
-         if(recorte == NULL) {
-            printf("Nenhum aluno encontrado.\n");
+         if(encontrados == 0) {
+            printf("+------+----------------------+-----+--------+\n");
+            printf("|          Nenhum aluno encontrado!          |\n");
+            printf("+------+----------------------+-----+--------+\n");
          }
          else {
             printf("+------+----------------------+-----+--------+\n");
-            printf("| Matr | Nome                 | Sex | Altura |\n");
-            printf("+------+----------------------+-----+--------+\n");
-
-            for(i = 0; i <=qtd; i++) { //um loop para mostrar cada aluno no vetor
-               if(strstr (vAluno[i].nome, pesq) != NULL)
-               printf("| %4d | %-20s |  %c  | %6.2f |\n", vAluno[i].matr, vAluno[i].nome, vAluno[i].sexo, vAluno[i].alt);
-            }
-
-            printf("+------+----------------------+-----+--------+\n");
-            printf("| Total: %17d Alunos Encontrados|\n", qtd);
+            printf("| Total: %17d Alunos Encontrados|\n", encontrados);
             printf("+------+----------------------+-----+--------+\n");
          }
          break;
@@ -108,8 +114,8 @@ int main()
          break;
       }
       if (op!=0) system("pause");
-      //o if se tiver menos de duas linhas dс para fazer assim sem chaves
-      //esse dс um sinal de pausinha antes de voltar a fazer todo o switch
    }
    while(op != 0);
+
+   return 0;
 }
